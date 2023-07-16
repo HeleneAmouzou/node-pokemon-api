c375f966ca2aadf22649cda26e92657121645049
HeleneAmouzou <helene.amouzou@gmail.com>
2023-07-17 00:06:08 +0200

    feat: advanced requests

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -1,9 +1,33 @@
-const { error } = require('console')
+const { count } = require('console')
 const { Pokemon } = require('../db/sequelize')
-  
+const { Op } = require('sequelize')  
+
 module.exports = (app) => {
   app.get('/api/pokemons', (req, res) => {
-    Pokemon.findAll()
+    if(req.query.name) {
+      const name = req.query.name
+      const limit = parseInt(req.query.limit) || 5
+
+      if(name.length < 2) {
+        const message = 'Le terme de recherche doit contenir au mois 2 caractères.'
+        return res.status(400).json({ message })
+      }
+
+      return Pokemon.findAndCountAll({ 
+        where: { 
+          name: {
+            [Op.like]: `%${name}%`
+          }
+        },
+        order: ['name'],
+        limit: limit 
+      })
+      .then(({count, rows}) => {
+        const message = `Il y a ${count} pokémons qui correspondent au terme de recherche ${name}.`
+        res.json({ message, data: rows })
+      })
+    } else {
+      Pokemon.findAll({order: ['name']})
       .then(pokemons => {
         const message = 'La liste des pokémons a bien été récupérée.'
         res.json({ message, data: pokemons })
@@ -12,5 +36,6 @@ module.exports = (app) => {
         const message = 'La liste des pokémons n\'as pas pu être récupérée. Veuillez réessayer dans quelques instants.'
         res.status(500).json({message, data: error})
       })
+    }
   })
 }
